a646592cae7eb400842f7c410f9f3be475e47402
alexjaruczik <alex.jaruczik@ontariotechu.net>
2025-11-11 20:16:04 -0500

    reconfigured the repositories, turned HTML files into ejs, installed ejs.

---
+++ AssignmentPage/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Blues' });
+});
+
+router.get('/aboutme', function(req, res, next) {
+  res.render('aboutme', { title: 'Blues' });
+});
+
+router.get('/contactme', function(req, res, next) {
+  res.render('contactme', { title: 'Blues' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Blues' });
+});
+
+
+module.exports = router;
